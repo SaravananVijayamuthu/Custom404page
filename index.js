2f97559b84c012c6ce95269a199e339e26c54c19
Saravanan raghul <saravananraghul1820@gmail.com>
2020-09-15 18:57:39 +0530

    try

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get( "/example1", (req, res) => {
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname + '/404.html'));
 });
-app.use(`/.netlify/functions/index`, router);
+
 
 //listen
 const PORT = 3000;

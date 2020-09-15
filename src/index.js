c61509c06f44d7f00ed8187c44bc6c7240bed957
Saravanan raghul <saravananraghul1820@gmail.com>
2020-09-15 18:39:53 +0530

    .ignore

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+//set up the Express app
+const app = express();
+
+//Parse incoming req data
+app.use(express.json());
+
+// Routes
+app.get( "/", (req, res) => {
+    res.json({data: "Hi Checking Route"});
+});
+app.get( "/example1", (req, res) => {
+    res.json({data: "Hi Checking Route__1"});
+});
+
+// 404 Route
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname + '/404.html'));
+});
+app.use(`/.netlify/functions/index`, router);
+
+//listen
+const PORT = 3000;
+app.listen(PORT, console.log(` Server is running at ${PORT}`));
+
+module.exports = app;
